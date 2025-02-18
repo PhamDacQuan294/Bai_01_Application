56af9477c235a7a6b605f1cb0cb4840c2d9c471c
PhamDacQuan294 <quan93909@gmail.com>
2025-02-18 18:36:34 +0700

    Create form don vi

---
+++ Don_Vi/src/bai01/Don_Vi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bai01;
+
+/**
+ *
+ * @author FPT
+ */
+public class Don_Vi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
