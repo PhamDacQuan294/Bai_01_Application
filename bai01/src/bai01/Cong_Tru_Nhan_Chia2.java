5ba0a98792ffa95f5c2dca0f108ce5e4a6c0d2d6
nguyendangQ <quangviplang@gmail.com>
2025-02-21 15:20:44 +0700

    fix
    mau

--- bai01/src/bai01/Cong_Tru_Nhan_Chia2.java
+++ bai01/src/bai01/Cong_Tru_Nhan_Chia2.java
@@ -215,6 +215,8 @@ public class Cong_Tru_Nhan_Chia2 extends javax.swing.JFrame {
         });
         jPanel2.add(BTNnumber0);
 
+        BTNclear.setBackground(new java.awt.Color(255, 0, 51));
+        BTNclear.setForeground(new java.awt.Color(255, 255, 255));
         BTNclear.setText("C");
         BTNclear.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -231,6 +233,8 @@ public class Cong_Tru_Nhan_Chia2 extends javax.swing.JFrame {
         });
         jPanel2.add(BTNsubtract);
 
+        BTNequals.setBackground(new java.awt.Color(255, 204, 0));
+        BTNequals.setForeground(new java.awt.Color(255, 255, 255));
         BTNequals.setText("=");
         BTNequals.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
